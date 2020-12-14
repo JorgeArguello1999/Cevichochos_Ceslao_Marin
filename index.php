06e03da8ead0c1e5395aa71616a7cf81f556ebe3
Jorge_Alexander <jorge.arguello1999@gmail.com>
2020-12-14 11:40:29 -0500

    14/12/2020, Solo se añade el archivo /index.php/ en la rama origin/master Se esta preparando la estructura de arbol para desarrollar la pagian web Se comienza a pedir informacion al beneficiario

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Cevichochos de la Ceslao Marin</title>
+		<meta charset="utf-8"/>
+		<link rel="icon" type >
+	</head>
+	<body>
+		<h1>Hola</h1>
+	</body>
+</html>
