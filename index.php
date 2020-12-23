8f83f6760bf182558f7f54912ae8237f99a1373b
Jorge <Jorge.arguello1999@gmail.com>
2020-12-23 13:43:50 -0500

    En este commit se finaliza con el desarrollo del "SLIDEBAR" este admite resoluciones alto=600px y de ancho=1000px ademas descubri como ajustar la animacion al compas de las fotografias, recien inicio con las conversaciones con el dueño del negocio y las fotos que estan ahi solo estan de prueba

--- index.php
+++ index.php
@@ -13,13 +13,13 @@
     <header class="header">
       <div class="container logo-nav-container">
 
-        <!--Ajustar para que el logo se concentre en la mitad del menu y distribuit
-        todas las opciones por todo la parte inferior de esta imagen
+    <!--Ajustar para que el logo se concentre en la mitad del menu y distribuit
+        todas las opciones por todo la parte inferior de esta imagen-->
 
-        <img src="Imagenes/Logo.ico" alt="La imagen no esta soportada por el navegador">
+        <img src="Imagenes/Logo.jpg" alt="La imagen no esta soportada por el navegador">
 
-        -->
-        <a href="#" class="logo">LOGO</a>
+
+        <!-- <a href="#" class="logo">LOGO</a> -->
         <nav class="navegacion">
           <ul>
             <li><a href="#">Nuestro Menu</a></li>
@@ -31,7 +31,19 @@
       </div>
     </header>
 
-      <main class="main">
+     <main class="main">
+	
+        <div class="slider">
+                <ul>
+                        <li><img src="Imagenes/imagen_1.jpg"></li>
+                        <li><img src="Imagenes/imagen_2.jpg"></li>
+			<li><img src="Imagenes/imagen_2.1.jpg"></li>
+                       <!-- <li><img src="Imagenes/Logo.ico"/></li> -->
+			<li><img src="Imagenes/imagen_3.jpg"></li>
+                </ul>
+	</div>
+
+
         <div class="container">
           <h1>Hola, soy el titulo</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
