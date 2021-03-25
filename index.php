09049e8e54bfada69b93ba520b66025bb6f5acc8
Jorge <jorge.arguello1999@gmail.com>
2021-03-25 02:05:48 -0500

    Ya se tiene casi todo, falta una parte del slider despues de eso tengo que trabajar full time en el cuerpo, de ahi solo hay que arreglar el slider y el foter pero tengo un buen ritmo, es hora de dormir Fuck yeah

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="index.css">
     <link rel="stylesheet" href="cabecera.css">
     <link rel="stylesheet" href="slider.css">
+    <link rel="stylesheet" href="footer.css">
     
     <link rel="icon" href="imagenes/logo.png" type="image/png">
 
@@ -26,10 +27,10 @@
         <!--Listado de contenido-->
         <nav class="navegacion">
           <ul>
-            <li><a href="#">Nuestro Menu</a></li>
-            <li><a href="#">Nuestras Ofertas</a></li>
-            <li><a href="#">¿Quienes Somos?</a></li>
-            <li><a href="#">¿Donde nos Encontramos?</a></li>
+            <li><a href="#Nuestro_Menu">Nuestro Menu</a></li>
+            <li><a href="#Nuestras_Ofertas">Nuestras Ofertas</a></li>
+            <li><a href="#Quienes_Somos">¿Quienes Somos?</a></li>
+            <li><a href="#Donde_nos_Encontramos">¿Donde nos Encontramos?</a></li>
           </ul>
         
         </nav>
@@ -43,34 +44,91 @@
         <ul>
           <li><img src="imagenes/imagen_1.jpg"></li>
           <li><img src="imagenes/imagen_2.jpg"></li>
-			    <li><img src="imagenes/imagen_2.1.jpg"></li>
-			    <li><img src="imagenes/imagen_3.jpg"></li>
+	  <li><img src="imagenes/imagen_2.1.jpg"></li>
+	  <li><img src="imagenes/imagen_3.jpg"></li>
         </ul>
 	    </div>
 
 
         <!--Contenido-->
         <div class="container">
-          <h1>Hola, soy el titulo</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <h1 id="Nuestro_Menu">Nuestro Menu</h1>
+          <h1 id="Nuestras_Ofertas">Nuestras Ofertas</h1>
+	  <h1 id="Quienes_Somos">¿Quienes Somos?</h1>
+	  <h1 id="Donde_nos_Encontramos">¿Donde nos Encontramos?</a></h1>
         </div>
     
     </main>
+
+    <footer>
+    <div class="all-footer">
+
+      <div class="footer">
+	
+	<div class="colum_1">
+	  <h1>Mas informacion</h1>	  
+	  <p>Un restaurante de comida rica y barata, perfecta para pasar un momento en familia acompañados de una comida saludable, respetando los estandares de salubridad.</p>
+	</div>
+
+	<div class="colum_2">
+	  <h1>Contactanos</h1>	  
+	  
+	  <div class="row">
+	    <a href="#" target="_blank">
+	      <i class="fas fa-phone-alt"></i>
+	      <label>Llamanos o Mensajeanos al +593 984 775 776</label>
+	    </a>
+	  </div>
+
+	  <div class="row">
+	    <a href="https://www.google.com.ec/maps/place/Juan+de+Velasco,+Puyo/@-1.4865855,-78.0058003,144m/data=!3m1!1e3!4m5!3m4!1s0x91d3e0a798ee041b:0x373b669749505d30!8m2!3d-1.4846256!4d-78.006947" target="_blank">
+	      <i class="fas fa-map-marker-alt"></i>
+	      <label>Encuentranos en: Ceslao Marin y Juan de Velasco (Esquina)</label>
+	    </a>
+	  </div>
+
+	  <div class="row">
+	    <a href="https://www.facebook.com/jaqueline.ponceosejos" target="_blank">
+	      <i class="fab fa-facebook-f iconos"></i>
+	      <label>Escribenos por Facebook: "Jaqueline Osejos"</label>
+	    </a>
+	  </div>
+
+	  <div class="row">
+	    <a href="jorge@gmail.com" target="_blank">
+	      <i class="far fa-envelope"></i>
+	      <label>Envianos un Email</label>
+	    </a>
+	  </div>
+
+
+	</div>
+	
+	<div class="colum_3">
+
+	  <h1>Contenido</h1>
+
+	  <div class="row2">
+	    <a href="#" target="_blank">
+	      <i class="fas fa-mobile-alt"></i>
+	      <label>Llamanos</label>
+	    </a>
+	  </div>
+
+	</div>
+
+      </div>
+
+      <div class="footer_abajo">
+
+	<div class="derechos_autor">
+	Todos los derechos reservados | Jorge Arguello
+	</div>
+
+      </div>
+
+    </div>
+    </footer>
 
   </body>
 </html>
