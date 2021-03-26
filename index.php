ffa5a20ea2c257ba1b2cc2ce889398538d63bb1c
Jorge <jorge.arguello1999@gmail.com>
2021-03-26 02:59:25 -0500

    Todo por hoy voy a dormir ya tengo gran parte solo me falta ofertas y quienes somos, el menu ya solo faltan las imagenes y los precios

--- index.php
+++ index.php
@@ -54,14 +54,111 @@
         <div class="container">
           
           <!--Nuestro menu-->
-          <h1 id="Nuestro_Menu">Nuestro Menu</h1>
-          <p>Hola.</p>
+	  <img src="imagenes/logo.png">
+	  <h1 id="Nuestro_Menu">Nuestro Menu</h1>
+
+	  <!--Cevichochos Menu-->
+	 <div class="menu">
+	  <h2>Cevichochos</h2>
+	  <dl>
+	    <dt>Cevichochos con Cuero</dt>
+	    <dd>Chifles, tostado y canguil</dd>
+	    
+	    <dt>Cevichochos con Camarón</dt>
+	    <dd>Chifles, tostado y canguil</dd>
+
+	    <dt>Cevichochos con Atún</dt>
+	    <dd>Chifles, tostado y canguil</dd>
+
+	    <dt>Cevichochos con Pollo</dt>
+	    <dd>Chifles, tostado y canguil</dd>
+
+	    <dt>Cevichochos Mixtos</dt>
+	    <dd>Chifles, tostado y canguil</dd>
+	  </dl>
+	  <img src="imagen de un cevichocho">
+	 </div>
+
+	  <!--Desayunos Menu-->
+	  <div class="menu_dere">
+	  <h2>Desayunos</h2>
+	   <dl>
+	    <dt>Desayuno Continental</dt>
+	    <dd>Café, huevos y vaso de jugo</dd>
+
+	    <dt>Desayuno Completo</dt>
+	    <dd>Café pasado, sanduche y seco de carne</dd>
+	   </dl>
+	   <img src="imagen de un desayuno">
+	  </div>
+
+	 <!--Tostadas y Sanduches-->
+	 <div class="menu">
+	  <h2>Tostadas y Sanduches</h2>
+	  <dl>
+	    <dt>Tostada de Queso</dt>
+	    <dd>Pan y queso</dd>
+	    
+	    <dt>Tostada de Mortadela</dt>
+	    <dd>Pan y mortadela</dd>
+
+	    <dt>Tostada Mixta</dt>
+	    <dd>Pan, queso y mortadela</dd>
+
+	  </dl>
+	  <img src="imagen de un sanduche">
+	 </div>
+
+	  <!--Jugos y Batidos Menu-->
+	  <div class="menu_dere">
+	  <h2>Jugos y Batidos</h2>
+	  <ul>
+	    <li>Mora</li>
+	    <li>Papaya</li>
+	    <li>Piña</li>
+	    <li>Guayabana</li>
+	    <li>Tomate de árbol</li>
+	  </ul>
+	  <img src="imagen de un jugo y un batido">
+	  </div>
+
+	  <!--Para Beber Menu-->
+	  <div class="menu">
+	  <h2>Para Beber</h2>
+	  <ul>
+	    <li>Agua Aromatica</li>
+	    <li>Agua Simple</li>
+	    <li>Agua Personal</li>
+	    <li>Agua de Jamaica</li>
+	  </ul>
+	  <img src="imagen de aguas">
+	  </div>
+	
+	  <!--Para Picar Menu-->
+	  <div class="menu_dere">
+	  <h2>Para Picar</h2>
+	  <ul>
+	    <li>Chifles</li>
+	    <li>Tostado</li>
+	    <li>Canguil</li>
+	    <li>Humitas</li>
+	    <li>Empanadas</li>
+	  </ul>
+	  <img src="imagen de una empanada">
+	  </div>
+
+
 
           <h1 id="Nuestras_Ofertas">Nuestras Ofertas</h1>
+
+	  <!--Mapa del local-->
 	  <h1 id="Donde_nos_Encontramos">¿Donde nos Encontramos?</a></h1>
+	  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d303.8096452403381!2d-78.00586994325623!3d-1.4866462978634025!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91d3e1a699b8f2ab%3A0xc5f71806a455e937!2sBanco!5e1!3m2!1ses-419!2sec!4v1616733220185!5m2!1ses-419!2sec" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+
 	  <h1 id="Quienes_Somos">¿Quienes Somos?</h1>
         </div>
 
+	</div>
     </main>
 
     <footer>
